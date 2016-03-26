cbc31eff16b4bc0494062248eb53a113f00c50e2
Nathan Reed <reednj@gmail.com>
2016-03-26 18:25:34 +0800

    abstract setting the values on the client side

--- lib/page-websocket.rb
+++ lib/page-websocket.rb
@@ -10,18 +10,45 @@ class PageWebSocket < WebSocketHelper
 	def on_open
 		super
 
-		self.send('exec', {
-			:selector => '#js-status',
-			:method => 'text',
-			:content => 'text from server'
-		})
+		self.element('#js-status').text = '<i>hello</i>'
 
 	end
 
 	def on_close
 		super
 	end
+
+	def element(selector)
+		ClientElement.new(selector, self)
+	end
 	
+end
+
+class ClientElement
+	attr_accessor :selector
+	
+	def initialize(selector, client)
+		raise 'client must be a WebSocketHelper' unless client.is_a? WebSocketHelper
+
+		@client = client
+		self.selector = selector.to_s
+	end
+
+	def execute(method, content)
+		@client.send('exec', {
+			:selector => self.selector,
+			:method => method,
+			:content => content.to_s
+		})
+	end
+
+	def text=(s)
+		self.execute 'text', s
+	end
+
+	def html=(s)
+		self.execute 'html', s
+	end
 
 end
 
